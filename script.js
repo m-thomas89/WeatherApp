0a4062e65684bfee103c1dead02c56b16947c717
Michael Thomas <m-thomas89@hotmail.com>
2020-10-13 21:42:50 -0400

    list of variables in JS

---
+++ script.js
@@ -0,0 +1,11 @@
+// List of variables going to be used
+var city="";
+var sCity = $("#search-city");
+var sButton = $("#search-button");
+var cButton = $("#clear-history");
+var cCity = $("#current-city");
+var cTemp = $("#temperature");
+var cHumidity= $("#humidity");
+var cWSpeed=$("#wind-speed");
+var cUVIndex= $("#uv-index");
+var cCity=[];
